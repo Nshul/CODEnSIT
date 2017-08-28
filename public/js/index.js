a981bd18518100810e531696ebc459e0fa066231
Anshul Mittal <anshulmittal0409@gmail.com>
2017-08-28 20:04:07 +0530

    Updated index page
    Signed-off-by: Anshul Mittal <anshulmittal0409@gmail.com>

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,4 @@
-$(document)
-    .ready(function() {
-
+ $(document).ready(function() {
         // create sidebar and attach to menu open
-        $('.ui.sidebar')
-            .sidebar('attach events', '.toc.item');
-
+        $('.ui.sidebar').sidebar('attach events', '.toc.item');
     });
